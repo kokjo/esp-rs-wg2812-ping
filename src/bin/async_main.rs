26ef726c709ce1046d78089a568d1fcd3b156d53
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2025-03-03 16:06:23 +0100

    use spi periperal with dma instead of rmt for ws2812

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -1,28 +1,29 @@
 #![no_std]
 #![no_main]
 
-use alloc::{collections::BTreeMap, vec::Vec};
+use alloc::collections::BTreeMap;
 use embassy_executor::Spawner;
 use embassy_net::{udp::PacketMetadata, IpListenEndpoint, Runner, StackResources};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel};
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::{riscv, Async};
-use esp_hal::timer::timg::TimerGroup;
-use esp_hal::time::RateExtU32;
-use esp_hal::rng::Rng;
-use esp_hal::rmt::{self, PulseCode, Rmt, TxChannelAsync, TxChannelConfig, TxChannelCreatorAsync};
-use esp_hal::interrupt::{software::SoftwareInterruptControl, Priority};
-use esp_hal::gpio::{Input, Pull};
 use esp_hal::clock::CpuClock;
-use esp_hal_embassy::InterruptExecutor;
+use esp_hal::dma::{self, DmaChannelFor};
+use esp_hal::gpio::interconnect::PeripheralOutput;
+use esp_hal::gpio::{Input, Pull};
+use esp_hal::peripheral::Peripheral;
+use esp_hal::rng::Rng;
+use esp_hal::spi::{self};
+use esp_hal::time::RateExtU32;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{dma_buffers, Async};
 use esp_wifi::wifi::{Configuration, WifiEvent};
-use log::info;
 use esp_wifi::{
     init,
     wifi::{self, ClientConfiguration, WifiController, WifiDevice, WifiStaDevice},
     EspWifiController,
 };
+use log::info;
 
 extern crate alloc;
 
@@ -42,6 +43,56 @@ macro_rules! mk_static {
 enum Event {
     Ping,
     Click,
+}
+
+const NUM_PIXELS: usize = 300;
+
+struct WS2812Controller<'d> {
+    spidma: spi::master::SpiDmaBus<'d, Async>,
+}
+
+impl<'d> WS2812Controller<'d> {
+    pub fn new(
+        spi: impl Peripheral<P = impl spi::master::PeripheralInstance> + 'd,
+        pin: impl Peripheral<P = impl PeripheralOutput> + 'd,
+        channel: impl Peripheral<P = impl DmaChannelFor<spi::AnySpi>> + 'd,
+    ) -> Self {
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) =
+            dma_buffers!(3 * 3 * NUM_PIXELS);
+        let dma_rx_buf = dma::DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = dma::DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+        Self {
+            spidma: spi::master::Spi::new(
+                spi,
+                spi::master::Config::default().with_frequency(2500u32.kHz()),
+            )
+            .unwrap()
+            .with_mosi(pin)
+            .with_dma(channel)
+            .with_buffers(dma_rx_buf, dma_tx_buf)
+            .into_async(),
+        }
+    }
+
+    pub async fn send_pixels(
+        &mut self,
+        pixels: impl Iterator<Item = [u8; 3]>,
+    ) -> Result<(), spi::Error> {
+        let mut pulses = [0u8; 3 * 3 * NUM_PIXELS];
+        let mut bitoff = 0;
+        for pixel in pixels {
+            let pixel = ((pixel[1] as u32) << 16) | ((pixel[0] as u32) << 8) | (pixel[2] as u32);
+            for j in 0..24 {
+                let bitpulse = if (pixel >> j) & 1 == 1 { 0b110 } else { 0b100 };
+                for k in 0..3 {
+                    let bit = (bitpulse >> k) & 1;
+                    pulses[bitoff / u8::BITS as usize] |= bit << (7 - (bitoff % u8::BITS as usize));
+                    bitoff += 1;
+                }
+            }
+        }
+        self.spidma.write_async(&pulses).await
+    }
 }
 
 #[esp_hal_embassy::main]
@@ -64,33 +115,16 @@ async fn main(spawner: Spawner) {
 
     let mut rng = Rng::new(peripherals.RNG);
 
-    // setup and spawn ws2812 task
+    let ws2812_ctrl = mk_static!(
+        WS2812Controller<'_>,
+        WS2812Controller::new(peripherals.SPI2, peripherals.GPIO4, peripherals.DMA_CH0)
+    );
 
-    let rmt = Rmt::new(peripherals.RMT, 80.MHz()).unwrap().into_async();
-    let channel = rmt.channel0.configure(peripherals.GPIO4, TxChannelConfig {
-        clk_divider: 1,
-        idle_output_level: false,
-        idle_output: true,
-        .. Default::default()
-    }).unwrap();
-
-    esp_hal::interrupt::enable(
-        esp_hal::peripherals::Interrupt::RMT,
-        Priority::Priority14
-    ).unwrap();
-
-    // let sw_inters = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    // let high_prio_executor = mk_static!(
-    //     InterruptExecutor::<0>, InterruptExecutor::<0>::new(
-    //     sw_inters.software_interrupt0
-    // ));
-    // let high_prio_spawner = high_prio_executor.start(Priority::Priority15);
-    // high_prio_spawner.spawn(ws2812_task(events.receiver(), channel, rng.clone())).ok();
-    spawner.spawn(ws2812_task(events.receiver(), channel, rng.clone())).ok();
+    spawner.must_spawn(ws2812_task(events.receiver(), ws2812_ctrl, rng.clone()));
 
     // Start button task
     let btn = mk_static!(Input, Input::new(peripherals.GPIO9, Pull::None));
-    spawner.spawn(btn_task(btn, events.sender())).ok();
+    spawner.must_spawn(btn_task(btn, events.sender()));
 
     // Setup and spawn wifi stack
     let timg0 = TimerGroup::new(peripherals.TIMG0);
@@ -100,31 +134,18 @@ async fn main(spawner: Spawner) {
         init(timg0.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
     );
 
-    let (wifi_sta, wifi_ctrl) = wifi::new_with_mode(
-        esp_wifi_ctrl,
-        peripherals.WIFI,
-        WifiStaDevice
-    ).unwrap();
-    spawner.spawn(wifi_ctrl_task(wifi_ctrl)).ok();
+    let (wifi_sta, wifi_ctrl) =
+        wifi::new_with_mode(esp_wifi_ctrl, peripherals.WIFI, WifiStaDevice).unwrap();
+    spawner.must_spawn(wifi_ctrl_task(wifi_ctrl));
 
     // Setup and spawn Network stack
     let (stack, runner) = embassy_net::new(
         wifi_sta,
         embassy_net::Config::dhcpv4(Default::default()),
         mk_static!(StackResources<3>, StackResources::<3>::new()),
-        (rng.random() as u64) << 32 | rng.random() as u64
+        (rng.random() as u64) << 32 | rng.random() as u64,
     );
-    spawner.spawn(net_task(runner)).ok();
-
-    // esp_hal::interrupt::enable(
-    //     esp_hal::peripherals::Interrupt::WIFI_BB,
-    //     Priority::Priority10
-    // );
-
-    esp_hal::interrupt::enable(
-        esp_hal::peripherals::Interrupt::WIFI_MAC,
-        Priority::Priority10
-    );
+    spawner.must_spawn(net_task(runner));
 
     info!("Waiting for link...");
     loop {
@@ -157,7 +178,11 @@ async fn main(spawner: Spawner) {
         &mut tx_data,
     );
 
-    sock.bind(IpListenEndpoint { addr: None, port: 1337 }).unwrap();
+    sock.bind(IpListenEndpoint {
+        addr: None,
+        port: 1337,
+    })
+    .unwrap();
 
     let mut packet = [0u8; 1024];
 
@@ -171,13 +196,13 @@ async fn main(spawner: Spawner) {
 
 #[embassy_executor::task]
 async fn wifi_ctrl_task(mut wifi_ctrl: WifiController<'static>) {
-    wifi_ctrl.set_configuration(
-        &Configuration::Client(ClientConfiguration{
+    wifi_ctrl
+        .set_configuration(&Configuration::Client(ClientConfiguration {
             ssid: SSID.try_into().unwrap(),
             password: PASSWORD.try_into().unwrap(),
-            .. Default::default()
-        })
-    ).unwrap();
+            ..Default::default()
+        }))
+        .unwrap();
 
     wifi_ctrl.start_async().await.unwrap();
 
@@ -200,9 +225,11 @@ async fn net_task(mut runner: Runner<'static, WifiDevice<'static, WifiStaDevice>
 }
 
 #[embassy_executor::task]
-async fn ws2812_task(events: channel::Receiver<'static, CriticalSectionRawMutex, Event, 10>, mut channel: rmt::Channel<Async, 0>, mut rng: Rng) {
-    const NUM_LEDS: i32 = 300;
-
+async fn ws2812_task(
+    events: channel::Receiver<'static, CriticalSectionRawMutex, Event, 10>,
+    ws2812_ctrl: &'static mut WS2812Controller<'static>,
+    mut rng: Rng,
+) {
     let mut lights: BTreeMap<i32, [u8; 3]> = BTreeMap::new();
 
     let colors = [
@@ -221,87 +248,34 @@ async fn ws2812_task(events: channel::Receiver<'static, CriticalSectionRawMutex,
 
     let mut dir = 1;
 
-    const NUM_SAMPLES: usize = 16;
-    let mut mins = [0u64; NUM_SAMPLES];
-    let mut avgs = [0u64; NUM_SAMPLES];
-    let mut maxs = [0u64; NUM_SAMPLES];
+    loop {
+        let pixels = (0..NUM_PIXELS as i32).map(|i| lights.get(&i).cloned().unwrap_or_default());
 
-    for i in 0.. {
-        let pixels = (0..NUM_LEDS).map(|i| lights.get(&i).cloned().unwrap_or_default());
+        ws2812_ctrl.send_pixels(pixels).await.unwrap();
 
-        let (min, avg, max) = send_pixels(&mut channel, pixels).await.unwrap();
+        lights = lights
+            .into_iter()
+            .filter_map(|(i, rgb)| (0 <= i && i <= NUM_PIXELS as i32).then_some((i + dir, rgb)))
+            .collect();
 
-        mins[i % mins.len()] = min;
-        avgs[i % mins.len()] = avg;
-        maxs[i % maxs.len()] = max;
-        if i % NUM_SAMPLES == 0 {
-            info!("num_samples = {} min_min = {} avg_min = {} max_min = {} min_avg = {} avg_avg = {} max_avg = {} min_max = {} avg_max = {} max_max = {}",
-                NUM_SAMPLES,
-                mins.iter().min().unwrap(),
-                mins.iter().sum::<u64>() / (mins.len() as u64),
-                mins.iter().max().unwrap(),
-
-                avgs.iter().min().unwrap(),
-                avgs.iter().sum::<u64>() / (avgs.len() as u64),
-                avgs.iter().max().unwrap(),
-
-                maxs.iter().min().unwrap(),
-                maxs.iter().sum::<u64>() / (maxs.len() as u64),
-                maxs.iter().max().unwrap()
-            );
-        }
-
-        lights = lights.into_iter().filter_map(|(i, rgb)| (0 <= i && i <= NUM_LEDS).then_some((i + dir, rgb))).collect();
-
-        let event = events.try_receive();        
+        let event = events.try_receive();
 
         if event == Ok(Event::Ping) || lights.is_empty() {
             let color = colors[(rng.random() as usize) % colors.len()];
-            lights.insert((dir == 1).then_some(0).unwrap_or(NUM_LEDS), color);
+            lights.insert((dir == 1).then_some(0).unwrap_or(NUM_PIXELS as i32), color);
         } else if event == Ok(Event::Click) {
             dir = -dir;
         }
 
-//        info!("{:?}", lights);
-        
-        Timer::after(Duration::from_millis(10)).await;
+        Timer::after(Duration::from_millis(1)).await;
     }
-}
-
-fn make_pixel_pulses(pixel: [u8; 3]) -> [u32; 25] {
-    let mut pulses = [0u32; 25];
-    let pixel = ((pixel[1] as u32) << 16) | ((pixel[0] as u32) << 8) | (pixel[2] as u32);
-    for i in 0..24 {
-        if (pixel >> (23-i)) & 1 == 0 {
-            pulses[i] = PulseCode::new(true, 32, false, 68);
-        } else {
-            pulses[i] = PulseCode::new(true, 68, false, 32);
-        }
-    }
-    pulses
-}
-
-async fn send_pixels(channel: &mut impl TxChannelAsync, pixels: impl Iterator<Item = [u8; 3]>) -> Result<(u64, u64, u64), esp_hal::rmt::Error> {
-    let mut times = Vec::with_capacity(500);
-    for pixel in pixels {
-        times.push(embassy_time::Instant::now().as_ticks());
-        channel.transmit(&make_pixel_pulses(pixel)).await.unwrap();
-    }
-
-    let (mut sum, mut cnt, mut min, mut max) = (0, 0, u64::MAX, u64::MIN);
-    for time in times.windows(2).map(|x| x[1] - x[0]) {
-        sum += time;
-        cnt += 1;
-        min = min.min(time);
-        max = max.max(time);
-    }
-    let avg = sum / cnt;
-
-    Ok((min, avg, max))
 }
 
 #[embassy_executor::task]
-async fn btn_task(btn: &'static mut Input<'static>, events: channel::Sender<'static, CriticalSectionRawMutex, Event, 10>) {
+async fn btn_task(
+    btn: &'static mut Input<'static>,
+    events: channel::Sender<'static, CriticalSectionRawMutex, Event, 10>,
+) {
     loop {
         btn.wait_for_falling_edge().await;
         info!("Button clicked!");
